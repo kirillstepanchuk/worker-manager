95abe0d696432e5acfb4688673d252cfb7c6b444
kirillstepanchuk <kirill.stepanchuk.96@mail.ru>
2021-12-08 11:40:39 +0300

    remove info of another branch

--- app-frontend/webpacks/webpack.common.js
+++ app-frontend/webpacks/webpack.common.js
@@ -2,46 +2,42 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const minifyHtmlPluginParameters = {
-  collapseWhitespace: true,
-  removeComments: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  useShortDoctype: true,
+	collapseWhitespace: true,
+	removeComments: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	useShortDoctype: true,
 };
 
 module.exports = (env) => {
-  const isProd = env === 'prod';
+	const isProd = env === 'prod';
 
-  return {
-    entry: './src/index.js',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(ttf|eot|woff|woff2)$/,
-          loader: 'file-loader',
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        minify: isProd && minifyHtmlPluginParameters,
-      }),
-    ],
-    // eslint: {
-    //     configFile: './.eslintrc.js',
-    //     emitWarning: true
-    // }
-  };
+	return {
+		entry: './src/index.js',
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: ['babel-loader'],
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.(ttf|eot|woff|woff2)$/,
+					loader: 'file-loader',
+				},
+			],
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+				minify: isProd && minifyHtmlPluginParameters,
+			}),
+		],
+	};
 };
--- app-frontend/webpacks/webpack.prod.js
+++ app-frontend/webpacks/webpack.prod.js
@@ -5,20 +5,20 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const common = require('./webpack.common');
 
 module.exports = merge(common('prod'), {
-  mode: 'production',
-  output: {
-    filename: '[chunkhash].js',
-    path: path.resolve(__dirname, '../dist'),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-    minimizer: [
-      new TerserPlugin(),
-    ],
-  },
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ],
+	mode: 'production',
+	output: {
+		filename: '[chunkhash].js',
+		path: path.resolve(__dirname, '../dist'),
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+		minimizer: [
+			new TerserPlugin(),
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin(),
+	],
 });
