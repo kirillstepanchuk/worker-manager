95d69169e2dc0dbaf19c521bf32441d09697c0ac
kirillstepanchuk <kirill.stepanchuk.96@mail.ru>
2022-01-27 20:46:40 +0300

    fix deploy problems

--- app-frontend/src/store/sagas/api/worker.api.ts
+++ app-frontend/src/store/sagas/api/worker.api.ts
@@ -1,24 +1,17 @@
-import axios from 'axios';
-
 import {
   Worker, WorkerData, WorkerEdit,
 } from '../../../types/worker';
 import { EditWorkerData } from '../../actions/editWorkerData/editWorkerData';
-import { API_URL } from '../../../constants';
+import axiosInstance from '../../../config';
 
 export const loadWorker = async (id: string): Promise<Worker> => {
-  const response = await axios({
-    url: `${API_URL}/workers/${id}`,
-    method: 'GET',
-  });
+  const response = await axiosInstance.get(`/workers/${id}`);
 
   return response.data;
 };
 
 export const addWorker = async (workerData: FormData): Promise<WorkerData> => {
-  const response = await axios({
-    method: 'POST',
-    url: `${API_URL}/workers`,
+  const response = await axiosInstance.post('/workers', {
     data: workerData,
     withCredentials: true,
   });
@@ -27,9 +20,7 @@ export const addWorker = async (workerData: FormData): Promise<WorkerData> => {
 };
 
 export const editWorker = async (action: EditWorkerData): Promise<WorkerEdit> => {
-  const response = await axios({
-    method: 'PATCH',
-    url: `${API_URL}/workers/${action.payload.id}`,
+  const response = await axiosInstance.patch(`/workers/${action.payload.id}`, {
     data: action.payload.workerData,
   });
 
--- app-frontend/src/store/sagas/api/workers.api.ts
+++ app-frontend/src/store/sagas/api/workers.api.ts
@@ -1,21 +1,16 @@
-import axios from 'axios';
-
 import { Worker } from '../../../types/worker';
 import { LoadWorkersDataPayload } from '../../actions/loadWorkersData/loadWorkersData';
-import { API_URL } from '../../../constants';
+import axiosInstance from '../../../config';
 
 // eslint-disable-next-line import/prefer-default-export
 export const loadWorkers = async (payload: LoadWorkersDataPayload): Promise<Worker[]> => {
-  const url = new URL(`${API_URL}/workers`);
+  const url = new URL('/workers');
   url.searchParams.append('pageNumber', String(payload.page));
   url.searchParams.append('positionType', String(payload.filterParameters?.positionType));
   url.searchParams.append('sortingType', String(payload.filterParameters?.sortingType as string));
   url.searchParams.append('time', String(payload.filterParameters?.time));
 
-  const response = await axios({
-    url: url.toString(),
-    method: 'GET',
-  });
+  const response = await axiosInstance.get(url.toString());
 
   return response.data;
 };
